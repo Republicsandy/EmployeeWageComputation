2d0267e48845e804d5391b949b90d497b6c72719
Republicsandy <sandy180388@gmail.com>
2022-08-18 15:34:17 +0530

    [sandeep] Emp absent and present file added in UC_1

---
+++ Employe_Wage_Computation/Emp_Absent_Present.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employe_Wage_Computation
+{
+    class EmployeePresentAbsent
+    {
+        public void EmpPrsentAbsent()
+        {
+            //Constant
+            const int IS_Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            //Computation
+
+            if (empCheck == IS_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Adsent");
+            }
+        }
+    }
+}
---
+++ Employe_Wage_Computation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employe_Wage_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WellCome TO Wmploye Wage Computation Program");
+
+            EmployeePresentAbsent employee = new EmployeePresentAbsent();
+            employee.EmpPrsentAbsent();
+        }
+    }
+}
